3c77e6aac5945566830983ff41640f38a2d5c559
Ian Shaw <ian.shaw1991@gmail.com>
2024-05-09 12:13:40 +0100

    Populate inventory slot icon image and added example textures to support this

--- Source/ExampleInventory/InventoryWidget.cpp
+++ Source/ExampleInventory/InventoryWidget.cpp
@@ -34,16 +34,14 @@ void UInventoryWidget::DrawInventory()
 	// Loop around the conventional inventory and add each element to the grid panel
 	for (int32 Index = 0; Index != EIC->Inventory.Num(); ++Index)
 	{
-		TSubclassOf<AItem> aitem_subclass = EIC->NearbyInventory[Index];
-		AItem* item_as_item = aitem_subclass.GetDefaultObject();
+		AItem* item_as_item = EIC->NearbyInventory[Index].GetDefaultObject();
 		AddItemToGridPanel(GP_Inventory, item_as_item, Index);
 	}
 
 	// Loop around the characters NearbyInventory and add each element to the nearby inventory grid panel
 	for (int32 Index = 0; Index != EIC->NearbyInventory.Num(); ++Index)
 	{		
-		TSubclassOf<AItem> aitem_subclass = EIC->NearbyInventory[Index];
-		AItem* item_as_item = aitem_subclass.GetDefaultObject();
+		AItem* item_as_item = EIC->NearbyInventory[Index].GetDefaultObject();
 		AddItemToGridPanel(GP_NearbyInventory, item_as_item, Index);
 	}
 }
@@ -58,6 +56,9 @@ void UInventoryWidget::AddItemToGridPanel(UUniformGridPanel* GridPanel, AItem* I
 
 	// Set the name of the item according to the class.
 	item_slot_as_inventory_slot_widget->TextBlockName->SetText(Item->ItemName);
+
+	// Set the icon of the item according to the class.
+	item_slot_as_inventory_slot_widget->ImageIcon->SetBrushFromTexture(Item->ItemIcon, true);
 
 	// Add the slot widget to the inventory grid panel
 	GridPanel->AddChildToUniformGrid(item_slot_as_inventory_slot_widget, Row, 0);
